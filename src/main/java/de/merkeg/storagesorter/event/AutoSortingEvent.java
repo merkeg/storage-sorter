4afaba939bab8d7498e3c15341eff3a43608433f
Egor Merk <egor@merkeg.de>
2025-07-11 08:27:15 +0200

    Read double chests as double chests

--- src/main/java/de/merkeg/storagesorter/event/AutoSortingEvent.java
+++ src/main/java/de/merkeg/storagesorter/event/AutoSortingEvent.java
@@ -8,12 +8,18 @@ import de.merkeg.storagesorter.config.Config;
 import de.merkeg.storagesorter.util.InventoryUtil;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.block.*;
+import net.minecraft.block.entity.BarrelBlockEntity;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.ChestBlockEntity;
+import net.minecraft.block.enums.ChestType;
+import net.minecraft.inventory.DoubleInventory;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 
 import java.util.*;
@@ -53,7 +59,12 @@ public class AutoSortingEvent implements ServerTickEvents.StartWorldTick {
       Inventory controllerInventory = (Inventory) world.getBlockEntity(storageSystem.getController());
 
       Set<Inventory> storage = new HashSet<>();
+      Set<BlockPos> visited = new HashSet<>();
       for(BlockPos blockPos : storageSystem.getStorage()) {
+
+        if(visited.contains(blockPos)) continue;
+        visited.add(blockPos);
+
         BlockState blockState = world.getBlockState(blockPos);
         if(!Config.getStorageBlocks().contains(blockState.getBlock())) {
           StorageSorter.LOGGER.warn("Storage block was destroyed, removing block");
@@ -62,7 +73,29 @@ public class AutoSortingEvent implements ServerTickEvents.StartWorldTick {
           break;
         }
 
-        storage.add((Inventory) world.getBlockEntity(blockPos));
+
+        BlockEntity blockEntity = world.getBlockEntity(blockPos);
+        Inventory inventory = null;
+        if (blockEntity instanceof ChestBlockEntity chest) {
+          inventory = ChestBlock.getInventory((ChestBlock) blockState.getBlock(), blockState, world, blockPos, false);
+
+          if(blockState.get(ChestBlock.CHEST_TYPE) != ChestType.SINGLE) {
+            Direction face = ChestBlock.getFacing(blockState);
+            BlockEntity neighbour = world.getBlockEntity(blockPos.offset(face));
+            if(neighbour instanceof ChestBlockEntity) {
+              visited.add(neighbour.getPos());
+            }
+          }
+
+        } else if (blockEntity instanceof BarrelBlockEntity barrel) {
+          inventory = barrel;
+        }
+
+        if(inventory != null) {
+          storage.add(inventory);
+        }
+
+
       }
 
       Map<Item, Set<Inventory>> lut = generateLUT(storage);
--- src/main/java/de/merkeg/storagesorter/util/BlockUtil.java
+++ src/main/java/de/merkeg/storagesorter/util/BlockUtil.java
@@ -3,6 +3,7 @@ package de.merkeg.storagesorter.util;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 
 import java.util.HashSet;
@@ -43,7 +44,50 @@ public class BlockUtil {
         }
       }
     }
-
     return visited;
+  }
+
+  public static Set<BlockPos> findSingleChestPositions(World world, BlockPos startPos) {
+    Set<BlockPos> connected = findConnectedBlocksOfSameType(world, startPos);
+    Set<BlockPos> result = new HashSet<>();
+    Set<BlockPos> processed = new HashSet<>();
+
+    for (BlockPos pos : connected) {
+      if (processed.contains(pos)) continue;
+
+      BlockState state = world.getBlockState(pos);
+      Block block = state.getBlock();
+
+      // Prüfe Nachbarn auf Doppelkiste
+      BlockPos doubleChestNeighbor = null;
+      for (BlockPos neighborPos : new BlockPos[]{
+              pos.north(),
+              pos.south(),
+              pos.east(),
+              pos.west()
+      }) {
+        if (connected.contains(neighborPos)) {
+          BlockState neighborState = world.getBlockState(neighborPos);
+          if (neighborState.getBlock() == block) {
+            doubleChestNeighbor = neighborPos;
+            break;
+          }
+        }
+      }
+
+      if (doubleChestNeighbor != null) {
+        // Doppelkiste gefunden, füge nur eine Position hinzu (z.B. die mit kleinerer Koordinate)
+        BlockPos chosen = pos.compareTo(doubleChestNeighbor) < 0 ? pos : doubleChestNeighbor;
+        result.add(chosen);
+        processed.add(pos);
+        processed.add(doubleChestNeighbor);
+      } else {
+        // Einzelkiste
+        result.add(pos);
+        processed.add(pos);
+      }
+    }
+
+    return result;
   }
 }
